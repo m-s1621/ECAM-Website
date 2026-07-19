9412f373e1dac2254a34306e17e486c677ee0f07
Meba Shimelis <96027075+m-s1621@users.noreply.github.com>
2026-07-19 15:44:22 -0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,12 @@ const modal = document.getElementById('modal');
 const modalImg = document.getElementById('modal-img');
 const commEventGallery = document.getElementById("comm-event-gallery");
 const fundraisingGallery = document.getElementById("fundraising-gallery");
+const ethiopianDayGallery = document.getElementById("ethiopian-day-gallery");
+const HealthScreeningGallery1 = document.getElementById("health-screening-gallery-1");
+const HealthScreeningGallery2 = document.getElementById("health-screening-gallery-2");
+const HealthScreeningGallery3 = document.getElementById("health-screening-gallery-3");
+const HealthScreeningGallery4 = document.getElementById("health-screening-gallery-4");
+
 
 const commEventImages =[
     { high: "comm-event70.webp", low: "comm-event70_lowquality.webp" },
@@ -105,16 +111,136 @@ const commEventImages =[
     "fundraising_3890.webp"
    ]
 
+   ethiopianDayImages= [
+    "GW7A7511.jpg",
+    "GW7A7518.jpg",
+    "GW7A7561.jpg",
+    "GW7A7566.jpg",
+    "GW7A7569.jpg",
+    "GW7A7579.jpg", 
+    "GW7A7583.jpg",
+    "GW7A7585.jpg",
+    "GW7A7592.jpg", 
+    "GW7A7597.jpg", 
+    "GW7A7598.jpg", 
+    "GW7A7612.jpg", 
+    "GW7A7619.jpg", 
+    "GW7A7624.jpg", 
+    "GW7A7631.jpg", 
+    "GW7A7646.jpg",
+    "GW7A7662.jpg",
+    "GW7A7671.jpg", 
+    "GW7A7676.jpg", 
+    "GW7A7682.jpg", 
+    "GW7A7691.jpg", 
+    "GW7A7695.jpg", 
+    "GW7A7696.jpg", 
+    "GW7A7703.jpg", 
+    "GW7A7705.jpg", 
+    "GW7A7719.jpg", 
+    "GW7A7722.jpg", 
+    "GW7A7726.jpg", 
+    "GW7A7729.jpg", 
+    "GW7A7739.jpg", 
+    "GW7A7740.jpg", 
+    "GW7A7742.jpg", 
+    "GW7A7745.jpg", 
+    "GW7A7750.jpg", 
+    "GW7A7751.jpg", 
+    "GW7A7760.jpg", 
+    "GW7A7766.jpg", 
+    "GW7A7770.jpg", 
+    "GW7A7774.jpg", 
+    "GW7A7780.jpg", 
+    "GW7A7783.jpg",
+    "GW7A7526.jpg",
+    "GW7A7527.jpg",
+    "GW7A7539.jpg",
+    "GW7A7543.jpg",
+    "GW7A7546.jpg",
+    "GW7A7553.jpg",
+    "GW7A7602.jpg",
+    "GW7A7604.jpg",
+    "GW7A7608.jpg",
+    "GW7A7626.jpg",
+    "GW7A7634.jpg",
+    "GW7A7651.jpg",
+    "GW7A7665.jpg",
+    "GW7A7679.jpg",
+    "GW7A7713.jpg",
+    "GW7A7723.jpg",
+    "GW7A7735.jpg",
+    "GW7A7754.jpg",
+    "GW7A7756.jpg"
+    
+   ]
+
+   HealthScreeningImages1 =[
+    "health_screening_pictures_1.jpg",
+    "health_screening_pictures_2.jpg",
+    "health_screening_pictures_3.jpg",
+    "health_screening_pictures_4.jpg",
+    "health_screening_pictures_5.jpg",
+    "health_screening_pictures_6.jpg",
+    "health_screening_pictures_7.jpg",
+    "health_screening_pictures_8.jpg",
+    "health_screening_pictures_9.jpg",
+    "health_screening_pictures_10.jpg",
+    "health_screening_pictures_11.jpg",
+    "health_screening_pictures_12.jpg",
+    "health_screening_pictures_13.jpg",
+    "health_screening_pictures_14.jpg",
+    "health_screening_pictures_15.jpg",
+    "health_screening_pictures_16.jpg",
+    "health_screening_pictures_17.jpg",
+    "health_screening_pictures_18.jpg",
+    "health_screening_pictures_19.jpg",
+    "health_screening_pictures_20.jpg",
+    "health_screening_pictures_21.jpg",
+    "health_screening_pictures_22.jpg",
+    "health_screening_pictures_23.jpg",
+    "health_screening_pictures_24.jpg",
+    "health_screening_pictures_25.jpg",
+    "health_screening_pictures_26.jpg",
+    "health_screening_pictures_27.jpg",
+    "health_screening_pictures_28.jpg",
+    "health_screening_pictures_29.jpg",
+    "health_screening_pictures_30.jpg",
+    "health_screening_pictures_31.jpg",
+    "health_screening_pictures_32.jpg",
+    "health_screening_pictures_33.jpg",
+    "health_screening_pictures_34.jpg",
+    "health_screening_pictures_35.jpg",
+    "health_screening_pictures_36.jpg",
+    "health_screening_pictures_37.jpg",
+    "health_screening_pictures_38.jpg",
+    "health_screening_pictures_39.jpg",
+    "health_screening_pictures_40.jpg",
+    "health_screening_pictures_41.jpg",
+    "health_screening_pictures_42.jpg",
+    "health_screening_pictures_43.jpg",
+    "health_screening_pictures_44.jpg",
+    "health_screening_pictures_45.jpg"
+  
+   ]
+
+   HealthScreeningImages2 = Array.from({length: 96}, (_, i) => `HealthScreeningImages_${i + 1}.jpg` );
+
+   HealthScreeningImages3 = Array.from({length: 101}, (_, i) => `HealthScreeningImages_${i + 1}.jpg` );
+
+   HealthScreeningImages4 = Array.from({length: 49}, (_, i) => `HealthScreeningImages_${i + 1}.jpg` );
+
+  
 
 function openModal(image) {
-  // console.log("openModal triggered");
+//   console.log("openModal triggered");
   document.getElementById('sticky-thumbnails').style.display = 'none';
   modal.style.display = 'flex';
   modalImg.src = image.src;
 }
 
 function closeModal() {
-  // console.log("closeModal triggered");
+//   console.log("closeModal triggered");
   modal.style.display = 'none';
   document.getElementById('sticky-thumbnails').style.display = 'block';
 }
@@ -175,10 +301,90 @@ fundraisingImages.forEach((imagePath) => {
 })
 
 
+ethiopianDayImages.forEach((imagePath) => {
+    const imgElement = document.createElement("img")
+    imgElement.src = `../images/event-gallery/Ethiopian_day_pics/${imagePath}`;
+     imgElement.loading = "lazy";
+    imgElement.classList.add("gallery-img");
+
+   
+    imgElement.addEventListener("click", function () {
+        openModal(this);
+    });
+
+    ethiopianDayGallery.appendChild(imgElement);
+    
+})
+
+
+
+  HealthScreeningImages1.forEach((imagePath) => {
+    const imgElement = document.createElement("img")
+    imgElement.src = `../images/event-gallery/Health_screening/${imagePath}`;
+     imgElement.loading = "lazy";
+    imgElement.classList.add("gallery-img");
+
+   
+    imgElement.addEventListener("click", function () {
+        openModal(this);
+    });
+
+    HealthScreeningGallery1.appendChild(imgElement);
+    
+  })
+
+  HealthScreeningImages2.forEach((imagePath) => {
+    const imgElement = document.createElement("img")
+    imgElement.src = `../images/event-gallery/Health_screening_2/${imagePath}`;
+    imgElement.loading = "lazy";
+    imgElement.classList.add("gallery-img");
+
+   
+    imgElement.addEventListener("click", function () {
+        openModal(this);
+    });
+
+    HealthScreeningGallery2.appendChild(imgElement);
+    
+  })
+
+  HealthScreeningImages3.forEach((imagePath) => {
+    const imgElement = document.createElement("img")
+    imgElement.src = `../images/event-gallery/Health_screening_3/${imagePath}`;
+    imgElement.loading = "lazy";
+    imgElement.classList.add("gallery-img");
+
+   
+    imgElement.addEventListener("click", function () {
+        openModal(this);
+    });
+
+    HealthScreeningGallery3.appendChild(imgElement);
+    
+  })
+
+  HealthScreeningImages4.forEach((imagePath) => {
+    const imgElement = document.createElement("img")
+    imgElement.src = `../images/event-gallery/Health_screening_4/${imagePath}`;
+    imgElement.loading = "lazy";
+    imgElement.classList.add("gallery-img");
+
+   
+    imgElement.addEventListener("click", function () {
+        openModal(this);
+    });
+
+    HealthScreeningGallery4.appendChild(imgElement);
+    
+  })
+
+  
+
 function thumbnailScroll() {
   const thumbnails = document.querySelectorAll('[data-target-id]');
 
   thumbnails.forEach(thumbnail => {
+
     thumbnail.addEventListener('click', function () {
       const targetId = this.getAttribute('data-target-id');
       const collapseEl = document.getElementById(targetId);
@@ -209,7 +415,6 @@ function thumbnailScroll() {
 document.addEventListener('DOMContentLoaded', () => {
   thumbnailScroll();
 
-
   // On page load, scroll to any open collapse
   const opened = document.querySelector('.collapse.show');
   if (opened) {
@@ -219,19 +424,23 @@ document.addEventListener('DOMContentLoaded', () => {
       window.scrollTo({ top: y, behavior: 'smooth' });
     }, 400);
   }
-
-  
 });
 
 
   
   // Call the function when the DOM is ready
   document.addEventListener('DOMContentLoaded', thumbnailScroll);
+  
+  
+   // promo video 
+  // const player = new Plyr('.js-player', {
+  //   controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
+  //   ratio: '12:7'
+  // });
+
+
+
+    var player = new MediaElementPlayer('#player1');
 
   
- // promo video 
-  const player = new Plyr('.js-player', {
-    controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
-    ratio: '12:7'
-  });
-
+ 
